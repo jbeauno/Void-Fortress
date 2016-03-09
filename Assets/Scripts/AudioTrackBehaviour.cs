75c326abf73888fb1bb453a945d8b97c136dbe9d
jbeauno <morinemm@yahoo.fr>
2016-03-09 09:32:31 +0100

    Audiotracks volume set by camera altitude

---
+++ Assets/Scripts/AudioTrackBehaviour.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AudioTrackBehaviour : MonoBehaviour {
+
+    public float p1Volume;
+    public float p1Altitude;
+
+    public float p2Volume;
+    public float p2Altitude;
+
+	void Start () {
+        	
+	}
+	
+
+	void Update () {
+	
+	}
+
+    public float GetVolume(float altitude)
+    {
+        float res = 0;
+
+        if (altitude <= p1Altitude)
+            res = p1Volume;
+        else if (altitude >= p2Altitude)
+            res = p2Volume;
+        else
+        {
+            //Calculer la pente y=ax+b
+
+            float pente = 0;
+            if ((p2Altitude - p1Altitude) != 0)
+                pente = (p2Volume - p1Volume) / (p2Altitude - p1Altitude);
+
+
+            res = p1Volume + pente * altitude;
+
+            print("pente: " + pente);
+            print("p1Volume: " + p1Volume);
+        }
+
+        print(altitude + " " + res);
+
+        return res;
+    }
+}
--- Assets/Scripts/GameAudio.cs
+++ Assets/Scripts/GameAudio.cs
@@ -20,52 +20,10 @@ public class GameAudio : MonoBehaviour {
         //todo: part 2: play fx .....        
 	}
 
-
-    private float getVolumeFromAltitude(float min, float max, float altitude)
-    //min < max, min: 1..10;  max: 1..10; 
-    //res : 0..-30
-    {
-        float res = -30;
-
-        float ainterval = max - min;
-        
-        if (altitude > max)
-            res = 0;
-        else if (altitude >= min)
-        {
-            res = -10;
-        }
-
-        return res;
-    }
-
     private void mixMusic()
     {
-        //todo: eventuellement avec snapshop
-
-
-        //altitude: 1 .. 10 
-        //bells  10..7 
-        //bass   3..1
-        //drum   5..1
-
-
-        float maxbell = 10;
-        float minbell = 7;
-
-        float maxbass = 3;
-        float minbass = 1;
-
-        float haxdrum = 5;
-        float mindrum = 1;
-
-        MixerMain.SetFloat("bells_volume", getVolumeFromAltitude(minbell, maxbell, Camera.altitude));
-
-        MixerMain.SetFloat("bass_fx_volume", -80);
-        MixerMain.SetFloat("drum_trad_volume", -80);
-
-
-        //MixerMain.SetFloat("bass_fx_volume", getVolumeFromAltitude(minbass, maxbass, Camera.altitude));
-        //MixerMain.SetFloat("drum_trad_volume", getVolumeFromAltitude(mindrum, haxdrum, Camera.altitude));
+        MixerMain.SetFloat("bells_volume", GameObject.Find("bells").GetComponent<AudioTrackBehaviour>().GetVolume(Camera.altitude));
+        MixerMain.SetFloat("bass_fx_volume", GameObject.Find("bassfx").GetComponent<AudioTrackBehaviour>().GetVolume(Camera.altitude));
+        MixerMain.SetFloat("drum_trad_volume", GameObject.Find("drumtrad").GetComponent<AudioTrackBehaviour>().GetVolume(Camera.altitude));       
     }
 }
